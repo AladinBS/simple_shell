4efe41af995408fc70b6f3cf41a2c5d7443c3aeb
AladinBS <aladin.bensassi@gmail.com>
2019-11-27 20:28:34 +0000

    f

--- sshell.c
+++ sshell.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
+
 /**
 * main - Entry point
-* @argc: arg count
-* @argv: list of args
+* @argc: argument count
+* @argv: list of arguments
 * @env: list of env vars
-* Return: Always 0
-* Aladin Bensassi / Kais Boumaiza
+* Return: Always 0 (Success)
 */
+
 int main(int argc, char *argv[], char **env)
 {
 char *ptr = NULL;
@@ -14,6 +15,7 @@ char **string = NULL;
 int status;
 pid_t parent;
 (void)argc;
+
 while (1)
 {
 ptr = NULL;
@@ -53,18 +55,19 @@ return (0);
 * prompt - prompts the user for commands
 * @ptr: ptr will hold the string of commands
 * Return: 1 for success
-* Aladin Bensassi / Kais Boumaiza
 */
+
 int prompt(char **ptr)
 {
 size_t size = 0;
-int x;
-if (isatty(STDIN_FIxO))
-write(STDOUT_FIxO, "Fuji$ ", 6);
-x = getline(ptr, &size, stdin);
-if (x == EOF)
+int len;
+
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "Fuji$ ", 6);
+len = getline(ptr, &size, stdin);
+if (len == EOF)
 free_cptrn(-1, 1, *ptr);
-(*ptr)[x - 1] = '\0';
+(*ptr)[len - 1] = '\0';
 if (*(*ptr) == '\0' || (*(*ptr) == '.' && (*ptr)[1] == '\0'))
 {
 free_cptrn(99, 1, *ptr);
